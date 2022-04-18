be89e25260c6ffccb7d28bed9e79a0441bcf28b5
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-04-18 18:17:01 +0900

    Updated Eldor's page

--- force-app/main/default/lwc/customComp/customComp.js
+++ force-app/main/default/lwc/customComp/customComp.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class CustomComp extends LightningElement {}
+export default class CustomComp extends LightningElement {
+    connectedCallback() {
+        setTimeout(()=>{
+            console.log('hi');
+        }, 1000);
+    }
+}
--- force-app/main/default/lwc/showTime/showTime.js
+++ force-app/main/default/lwc/showTime/showTime.js
@@ -1,15 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class ShowTime extends LightningElement {
-  
     time = new Date();
     intervalId;
-    
+
     connectedCallback() {
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
         this.intervalId = setInterval(() => {
             this.time = new Date();
         }, 1000);
     }
+
+    renderedCallback() {}
 
     disconnectedCallback() {
         clearInterval(this.intervalId);
